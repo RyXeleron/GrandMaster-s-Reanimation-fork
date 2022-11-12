c5ecaaf5bbfe7ce0835ad8db30972a2760c5fd0a
coolsk8rboy <95263848+coolsk8rboy@users.noreply.github.com>
2022-11-12 16:47:37 -0500

    Update JohnCenasMain.lua

--- JohnCenasMain.lua
+++ JohnCenasMain.lua
@@ -1,7 +1,7 @@
 local Flinging = false
 local Highlight = Instance.new("Highlight")
 local Root = game.Players.LocalPlayer.Character.HumanoidRootPart
-local Version = "v0.0.9"
+local Version = "v1.0.1"
 print(Version)
 
 Highlight.FillColor = Color3.fromRGB(34, 77, 153)
